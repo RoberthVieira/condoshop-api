2e84acd115e554ec8378d1497b21172f87fbe8da
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2026-07-13 22:50:02 -0300

    feat: implementa e organiza rotas moradores e produtos

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, {Request, Response} from 'express'
 import { env } from './config/env.js';
+import produtosRouter from './routes/produtosRouter.js';
+import moradoresRouter from './routes/moradoresRouter.js'
 
 const server = express();
 server.use(express.json());
@@ -12,13 +14,9 @@ server.get('/status', (req: Request, res: Response) => {
     res.status(200).json({status: 'online'})
 })
 
-server.get('/moradores', (req: Request, res: Response) => {
-    res.status(200).json({data: []});
-});
+server.use('/moradores', moradoresRouter);
 
-server.get('/produtos', (req: Request, res: Response) => {
-    res.status(201).json({data: []})
-})
+server.use('/produtos', produtosRouter);
 
 server.listen(env.PORT, () => {
     console.log(`${env.APP_NAME} rodando na porta ${env.PORT}`)
