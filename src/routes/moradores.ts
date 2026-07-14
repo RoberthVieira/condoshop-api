f219b320c2fdee6ebdf8d6cba8276925a87b7403
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2026-07-13 22:23:31 -0300

    correção de conflito

--- src/routes/moradores.ts
+++
@@ -1,8 +0,0 @@
-import { IncomingMessage, ServerResponse } from "http";
-import { responderSucesso } from "../utils/http.js";
-
-export function rotasMoradores(req: IncomingMessage, res: ServerResponse, method: string, url: string): void {
-    if(method === "GET" && url === '/moradores') {
-        responderSucesso(res, 200, {moradores: []})
-    }
-}
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,24 @@
-import http, { IncomingMessage, ServerResponse }  from 'http';
-import { rotasMoradores } from './routes/moradores.js';
-import { rotasProdutos } from './routes/produtos.js';
-import { respostaErro } from './utils/http.js';
+import express, {Request, Response} from 'express'
 import { env } from './config/env.js';
 
-const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    const method = req.method ?? 'GET';
-    const url = req.url ?? '/';
+const server = express();
+server.use(express.json());
 
-    console.log(`${req.method} ${req.url}`);
-
-    if(url.startsWith('/produtos')){
-        rotasProdutos(req, res, method, url);
-    } else if (url.startsWith('/moradores')){
-        rotasMoradores(req, res, method, url);
-    } else {
-        respostaErro(res, 404, "Rota Não encontrada!")
-    }
-    
-    
+server.get('/', (req: Request, res:Response) => {
+    res.status(200).json({data : {projeto: 'CondoShop', versao: '1.0'}});
 });
+
+server.get('/status', (req: Request, res: Response) => {
+    res.status(200).json({status: 'online'})
+})
+
+server.get('/moradores', (req: Request, res: Response) => {
+    res.status(200).json({data: []});
+});
+
+server.get('/produtos', (req: Request, res: Response) => {
+    res.status(201).json({data: []})
+})
 
 server.listen(env.PORT, () => {
     console.log(`${env.APP_NAME} rodando na porta ${env.PORT}`)
